3d63f0ea0e242b8ad47dc4f61e4ced6bd7687d9d
Aaron Seigo <aseigo@kde.org>
2015-12-07 21:21:48 +0100

    test the compress command

---
+++ test/eimap_command_compress_tests.erl
@@ -0,0 +1,52 @@
+%% Copyright 2014 Kolab Systems AG (http://www.kolabsys.com)
+%%
+%% Aaron Seigo (Kolab Systems) <seigo a kolabsys.com>
+%%
+%% This program is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-module(eimap_command_compress_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+parse_test_() ->
+    Data =
+    [
+        % { Binary Response, Binary Tag, Parsed Results }
+        {
+          <<"abcd OK DEFLATE active\r\n">>,
+          <<"abcd">>,
+          compression_active
+        },
+        {
+          <<"abcd BAD Uh uh uh\r\n">>,
+          <<"abcd">>,
+          { error, <<"Uh uh uh">> }
+        },
+        {
+          <<"abcd NO Uh uh uh\r\n">>,
+          <<"abcd">>,
+          { error, <<"Uh uh uh">> }
+        }
+    ],
+    lists:foldl(fun({ Response, Tag, Parsed }, Acc) -> [?_assertEqual(Parsed, eimap_command_compress:parse(Response, Tag))|Acc] end, [], Data).
+
+new_test_() ->
+    Data =
+    [
+        % input, output
+        { <<>>, <<"COMPRESS DEFLATE">> },
+        { true, <<"COMPRESS DEFLATE">> },
+        { [], <<"COMPRESS DEFLATE">> }
+    ],
+    lists:foldl(fun({ Params, Command }, Acc) -> [?_assertEqual(Command, eimap_command_compress:new(Params))|Acc] end, [], Data).
+
