ba39dc9c950f696769f4bf8fbe2d84daf6a96867
Aaron Seigo <aseigo@kde.org>
2015-12-07 23:53:01 +0100

    start testing namespae command
    much brokenness, needs a good looking over

---
+++ test/eimap_command_namespace_tests.erl
@@ -0,0 +1,72 @@
+%% Copyright 2014 Kolab Systems AG (http://www.kolabsys.com)
+%%
+%% Aaron Seigo (Kolab Systems) <seigo a kolabsys.com>
+%%
+%% This program is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-module(eimap_command_namespace_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+parse_test_() ->
+    Data =
+    [
+        % { Binary Response, Binary Tag, Parsed Results }
+        {
+          <<"* NAMESPACE ((\"\" \"/\")) NIL NIL\r\nabcd OK NAMESPACE command completed\r\n">>,
+          <<"abcd">>,
+          { fini, { none, none } }
+        },
+%        {
+%          <<"* NAMESPACE NIL NIL ((\"\" \".\"))\r\nabcd OK NAMESPACE command completed\r\n">>,
+%          <<"abcd">>,
+%          namespace
+%        },
+%        {
+%          <<"* NAMESPACE ((\"\" \"/\")) NIL ((\"Public Folders/\" \"/\"))\r\nabcd OK NAMESPACE command completed\r\n">>,
+%          <<"abcd">>,
+%          namespace
+%        },
+%        {
+%          <<"* NAMESPACE ((\"\" \"/\")) ((\"~\" \"/\")) ((\"#shared/\" \"/\")\r\n(\"#public/\" \"/\")(\"#ftp/\" \"/\")(\"#news.\" \".\"))\r\nabcd OK NAMESPACE command completed\r\n">>,
+%          <<"abcd">>,
+%          namespace
+%        },
+%        {
+%          <<"* NAMESPACE ((\"INBOX.\" \".\")) NIL  NIL\r\nabcd OK NAMESPACE command completed\r\n">>,
+%          <<"abcd">>,
+%          namespace
+%        },
+        {
+          <<"abcd BAD Uh uh uh\r\n">>,
+          <<"abcd">>,
+          { error, <<"Uh uh uh">> }
+        },
+        {
+          <<"abcd NO Uh uh uh\r\n">>,
+          <<"abcd">>,
+          { error, <<"Uh uh uh">> }
+        }
+    ],
+    lists:foldl(fun({ Response, Tag, Parsed }, Acc) -> [?_assertEqual(Parsed, eimap_command_namespace:parse(Response, Tag))|Acc] end, [], Data).
+
+new_test_() ->
+    Data =
+    [
+        % input, output
+        { <<>>, <<"NAMESPACE">> },
+        { true, <<"NAMESPACE">> },
+        { [], <<"NAMESPACE">> }
+    ],
+    lists:foldl(fun({ Params, Command }, Acc) -> [?_assertEqual(Command, eimap_command_namespace:new(Params))|Acc] end, [], Data).
+
