f8051a5e5b96565999680781b5e507c5ace4ec54
Aaron Seigo <aseigo@kde.org>
2015-12-10 20:41:17 +0100

    defaults

--- src/eimap.hrl
+++ src/eimap.hrl
@@ -1,2 +1,2 @@
--record(eimap_server_config, { host :: string(), port :: integer(), tls = true :: boolean() }).
+-record(eimap_server_config, { host :: string(), port = 143 :: integer(), tls = false :: boolean() }).
 
