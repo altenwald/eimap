06a37b85a5c56fb505946efb22f9dfff81843a4a
Aaron Seigo <aseigo@kde.org>
2016-06-13 11:32:32 +0200

    this is a library. use the LGPLv3+

--- test/eimap_command_noop_tests.erl
+++ test/eimap_command_noop_tests.erl
@@ -3,16 +3,16 @@
 %% Aaron Seigo (Kolab Systems) <seigo a kolabsys.com>
 %%
 %% This program is free software: you can redistribute it and/or modify
-%% it under the terms of the GNU General Public License as published by
+%% it under the terms of the GNU Library General Public License as published by
 %% the Free Software Foundation, either version 3 of the License, or
 %% (at your option) any later version.
 %%
 %% This program is distributed in the hope that it will be useful,
 %% but WITHOUT ANY WARRANTY; without even the implied warranty of
 %% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%% GNU General Public License for more details.
+%% GNU Library General Public License for more details.
 %%
-%% You should have received a copy of the GNU General Public License
+%% You should have received a copy of the GNU Library General Public License
 %% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -module(eimap_command_noop_tests).
